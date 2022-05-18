223fd915674c1f8e84bacd3616bb9cc2766db92a
Dimitar Mihaylov <dimitar.mihaylov@bg.ibm.com>
2022-05-18 15:26:04 +0300

    Implemented dynamic search using switchMap while typing into search field

--- source-code/ecommerce-project-release-3.0/19-payment-processing-with-stripe-email-support/03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
+++ source-code/ecommerce-project-release-3.0/19-payment-processing-with-stripe-email-support/03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -1,20 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {debounceTime, distinctUntilChanged, fromEvent, Observable, startWith, Subscription, switchMap} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+    searchBox = document.getElementById('searchBox') as HTMLInputElement;
 
-  doSearch(value: string) {
-    console.log(`value=${value}`);
-    this.router.navigateByUrl(`/search/${value}`);
-  }
+
+    @ViewChild('myInput')
+    input: ElementRef;
+    private subscription: Subscription;
+
+
+    constructor(private router: Router) {
+    }
+
+    ngAfterViewInit(): void {
+        const searchText$: Observable<string> =
+            fromEvent<any>(this.input.nativeElement, 'keyup')
+                .pipe(
+                    map(event => event.target.value),
+                    startWith(''),
+                    debounceTime(400),
+                    distinctUntilChanged()
+                );
+
+        this.subscription = searchText$.pipe(
+            switchMap(search => this.router.navigateByUrl(`/search/${search}`)
+            )).subscribe(x => console.log(x));
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    ngOnInit() {
+
+    }
+
+    doSearch(value: string) {
+        console.log(`value=${value}`);
+        this.router.navigateByUrl(`/search/${value}`);
+    }
+
+
 }
